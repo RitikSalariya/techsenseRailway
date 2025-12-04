72547035eec5fa058b1fc681c8354cfcc6626cd8
Ritik Salariya <ritik6598@gmail.com>
2025-12-04 23:25:19 +0530

    Email

--- techsense/settings.py
+++ techsense/settings.py
@@ -173,18 +173,19 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 # ---------------- EMAIL SETTINGS ----------------
 if DEBUG:
+    # Local dev: show emails in console
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 else:
+    # Production (Railway): real SMTP (Brevo / SendGrid / etc.)
     EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
-    EMAIL_HOST = "smtp-relay.brevo.com"
-    EMAIL_PORT = 587
+    EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp-relay.brevo.com")  # Brevo default
+    EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
     EMAIL_USE_TLS = True
 
-    EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-    EMAIL_HOST_PASSWORD = os.getenv("BREVO_API_KEY")
+    EMAIL_HOST_USER = os.getenv("9d4d5d001@smtp-brevo.com")#
+    EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")#E
     DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", EMAIL_HOST_USER)
-
 
 # DEBUG = True   # make sure this is True for testing
 
