b622d053319070d745edde52d265c815ef63e2af
Ritik Salariya <ritik6598@gmail.com>
2025-12-04 14:28:40 +0530

    Email

--- techsense/settings.py
+++ techsense/settings.py
@@ -160,4 +160,17 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # ----------------------------------------------------
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # For local development: print emails to console instead of sending
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+if DEBUG:
+    # Local dev: print emails in console
+    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+else:
+    # Production (Railway): real SMTP
+    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+    EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp.gmail.com")
+    EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
+    EMAIL_HOST_USER = os.getenv("techsensetechnogiesltd@gmail.com")
+    EMAIL_HOST_PASSWORD = os.getenv("khmzsddcxmykmkrx")
+    EMAIL_USE_TLS = True
+    DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", EMAIL_HOST_USER)
+
