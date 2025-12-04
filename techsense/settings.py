6337e04647b3ab0289a0687077504ae87656633d
Ritik Salariya <ritik6598@gmail.com>
2025-12-04 22:57:47 +0530

    Email

--- techsense/settings.py
+++ techsense/settings.py
@@ -184,7 +184,7 @@ else:
     EMAIL_USE_TLS = True
 
     EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")#
-    EMAIL_HOST_PASSWORD = os.getenv("BREVO_API_KEY")#EMAIL_HOST_PASSWORD
+    EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")#E
     DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", EMAIL_HOST_USER)
 
 # DEBUG = True   # make sure this is True for testing
